b57177598193ff90f64585a23368c876bd599afe
Manpreetkaur666 <mk27982@gmail.com>
2023-01-09 18:18:47 -0500

    edited about

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,22 +1,22 @@
 import React, {useContext} from 'react'
 import noteContext from '../context/notes/noteContext'
+import MainScreen from '../images/MainScreen.png'
 
 export const About = () => {
   const a = useContext(noteContext);
   return (
     <div className='about'>
-      <h1>iMemobook</h1>
+      <h1 className='aboutHeading'><strong>i<span>M</span>emo<span>B</span>ook</strong></h1>
       <div className='about-section'>
         <div>
-        <p>Write you important notes and save it in your account and log in to your account
+        <p>Write you important notes and save it in your account.
         Login to your account whenever wants to access your notes. Edit your already created notes
         and delete it if you no longer needed.
         Give Title to your note and add description and you can also add a tag to give your note a category.
       </p>
         </div>
-        <div>
-          <img src='/src/images/MainScreen.png'/>
-          
+        <div className='img-container'>
+          <img className='MainScreenImg' src={MainScreen}/>
         </div>
       
       </div>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -17,15 +17,15 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"><strong>iMemoBook</strong></a>
+    <a className="navbar-brand"><strong>i<span>M</span>emo<span>B</span>ook</strong></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <Link className={`nav-link ${location.pathname === "/"?"active":""}`} aria-current="page" to="/">Home</Link>
-        </li>
+        </li> */}
         <li className="nav-item">
           <Link className={`nav-link ${location.pathname === "/about"?"active":""}`} to="/about">About</Link>
         </li>
